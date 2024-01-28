654bc1c481943ce4bd43df37ac204e98afd6ec6d
saurabhthecodewizard <skmahajan4998@gmail.com>
2024-01-29 01:05:28 +0530

    compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+  // To allow the host to be exposed from container
+  preview: {
+    host: true,
+    port: 3000
+  }
 })
